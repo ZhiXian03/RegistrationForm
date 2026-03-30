c3def010b6016e34cb097d88658c9647200aeb7d
ZhiXian03 <zxiancheong03@gmail.com>
2026-03-30 15:14:05 +0800

    Update RegistrationForm.py

--- RegistrationForm.py
+++ RegistrationForm.py
@@ -11,7 +11,7 @@ password = st.text("Password", type="password")
 
 age = st.number_input("Age: ", min_value=1, max_value=100)
 
-gender = st.radio("Gender: ", ["Male", "Female"]
+gender = st.radio("Gender: ", ["Male", "Female"])
 
 course = st.selectbox("Select Course: ", ["Information Technology", "Business", "Engineering"])
 
